fa61b3b52e7b36af98d2c1f3340c2f28e3e41f46
mercy-shii <wemercy525@gmail.com>
2020-05-12 19:04:41 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("wmercy@gmail.com")
-    MAIL_PASSWORD = os.environ.get("evansnjufi")
-    SUBJECT_PREFIX = 'New Blog'
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 class ProdConfig(Config):
 
      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
