ab15bbbf4184d08b106144b164e676ed46be86a4
mercy-shii <wemercy525@gmail.com>
2020-05-12 13:36:23 +0300

    styling of templates

--- app/main/views.py
+++ app/main/views.py
@@ -84,7 +84,7 @@ def new_blog(uname):
 @login_required
 def comments(blog_id):
     blog = Blog.query.filter_by(id = blog_id).first()
-    comments = Comment.query.filter_by(blog_id = blog.id).order_by(Comment.date_posted.desc())
+    comments = Comment.query.filter_by(blog_id = blog.id).order_by()
 
 
     return render_template('comments.html', blog = blog, comments = comments)
@@ -93,7 +93,7 @@ def comments(blog_id):
 @login_required
 def del_comment(blog_id, comment_id):
     blog = Blog.query.filter_by(id = blog_id).first()
-    comments = Comment.query.filter_by(blog_id = blog.id).order_by(Comment.date_posted.desc())
+    comments = Comment.query.filter_by(blog_id = blog.id).order_by()
     comment = Comment.query.filter_by(id = comment_id).first()
     if blog.user_id == current_user.id or comment.user_id == current_user.id:
 
