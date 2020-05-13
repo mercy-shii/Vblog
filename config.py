4125a4eb9b084ad446422f7c1ea39b5c57e4f1e9
mercy-shii <wemercy525@gmail.com>
2020-05-13 10:21:34 +0300

    database migration

--- config.py
+++ config.py
@@ -2,26 +2,31 @@ import os
 class Config:
     
     SECRET_KEY =('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mercy:shii@localhost/blogs'
+
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    
     
 class ProdConfig(Config):
 
-     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+       SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 
 class TestConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mercy:shii@localhost/blog_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mercy:shii@localhost/blogs_test'
     
 
 class DevConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mercy:shii@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mercy:shii@localhost/blogs'
     DEBUG = True
     
 
